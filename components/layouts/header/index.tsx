0e0c15e0b7d80e9731494017c6da97b1d56c16c4
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-01-22 18:56:51 +0800

    mobile view for promises

--- components/layouts/header/index.tsx
+++ components/layouts/header/index.tsx
@@ -1,11 +1,12 @@
 import { TopSetting } from "./topsetting";
 import Navbar from "./navbar";
+import styles from "./style.module.css";
 
 export const Header: React.FC = () => {
 	return (
-		<>
+		<div className={styles.container}>
 			<TopSetting />
 			<Navbar />
-		</>
+		</div>
 	);
 };
