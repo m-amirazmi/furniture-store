82078caca2d65eebf32eb3900da7b8551777bc4b
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-02-03 03:33:20 +0800

    add hero image

--- components/home/hero/index.tsx
+++ components/home/hero/index.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
 		<div className={styles.container}>
 			<div className={styles.heroimg}>
 				<div className={styles.bannerweb}>
-					<Image src="https://via.placeholder.com/1600x600.png?text=1600x600" alt="placeholder" layout="fill" objectFit="cover" />
+					<Image src="/assets/images/furnitures/hero.png" alt="placeholder" width={1680} height={630} objectFit="cover" />
 				</div>
 				<div className={styles.bannermob}>
 					<Image src="https://via.placeholder.com/600x600.png?text=600x600" alt="placeholder" layout="fill" objectFit="cover" />
